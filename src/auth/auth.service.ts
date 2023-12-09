0c3cf7e9620dda0d1374af727bfa6d319c027057
Kosteyyy <7456052@gmail.com>
2023-12-10 00:16:09 +0300

    jwt tocken signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,10 +4,12 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService) { }
 
   async signup(dto: AuthDto) {
     // generate pass hash
@@ -57,8 +59,20 @@ export class AuthService {
 
     if(!pwMatches) throw new ForbiddenException("Credentials incorrect");
 
-    delete user.hash;
+    return this.signToken(user.id, user.email);
+  }
 
-    return user;
+  signToken(userId: number, email: string) {
+    const payload = {
+      sub: userId,
+      email,
+
+    };
+    const secret = this.config.get('JWT_SECRET')
+
+    return this.jwt.signAsync(payload, 
+      {expiresIn: '15m',
+      secret
+    })
   }
 }
